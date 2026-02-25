4590ba869b45709e3dcb45262b37b109cd7b11c0
Bohan <fengb3@gmail.com>
2026-02-25 14:52:35 +0800

    build: move deploy to CI, remove local generated docker-compose & env

--- WHY.AppHost/AppHost.cs
+++ WHY.AppHost/AppHost.cs
@@ -8,19 +8,28 @@ builder
         dashboard.WithHostPort(8080).WithForwardedHeaders(enabled: true);
     });
 
-var postgres = builder.AddPostgres("postgres").WithDataVolume("why_postgres_data");
+var postgres = builder
+    .AddPostgres("postgres")
+    .WithDataVolume("why_postgres_data");
+postgres.PublishAsDockerComposeService((r, s) => { });
+
 var postgresDB = postgres.AddDatabase("postgresdb");
 
 var api = builder
     .AddProject<Projects.WHY_Api>("why-api")
     .WaitFor(postgresDB)
     .WithReference(postgresDB);
+api.PublishAsDockerComposeService((r, s) => { });
 
-var mcpRemote = builder
-    .AddProject<Projects.WHY_MCP_Remote>("why-mcp-remote")
+// var mcpRemote = builder
+//     .AddProject<Projects.WHY_MCP_Remote>("why-mcp-remote")
+//     .WaitFor(api)
+//     .WithReference(api);
+
+var web = builder
+    .AddProject<Projects.WHY_Web>("why-web")
     .WaitFor(api)
     .WithReference(api);
-
-var web = builder.AddProject<Projects.WHY_Web>("why-web").WaitFor(api).WithReference(api);
+web.PublishAsDockerComposeService((r, s) => { });
 
 builder.Build().Run();
